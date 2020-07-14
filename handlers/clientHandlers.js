d705e1a07519d17816b0ef88b69bb2e03dbb1980
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-14 18:25:58 -0400

    exercise 2

--- handlers/clientHandlers.js
+++ handlers/clientHandlers.js
@@ -1,4 +1,54 @@
-const { v4: uuidv4 } = require('uuid');
-const { clients } = require('../data/clients');
+const { v4: uuidv4 } = require("uuid");
+const { clients } = require("../data/clients");
 
-// write your handlers here...
+const handleClients = (req, res) => {
+  console.log(clients);
+  res.status(200).json(clients);
+};
+
+const handleAddClient = (req, res) => {
+  const newClient = { ...req.body, id: uuidv4() };
+  if (clients.filter((client) => client.email === newClient.email).length) {
+    res.status(400).json({
+      status: 400,
+      message: "The clients email already exists in our database",
+    });
+  } else {
+    clients.push(newClient);
+    res.status(201).json({
+      status: 201,
+      data: newClient,
+      message: "user created!!",
+    });
+  }
+};
+
+const handleClient = (req, res) => {
+  const id = req.params.id;
+  const result = clients.find((client) => {
+    if (client.id === id) {
+      return client;
+    }
+  });
+  if (result) {
+    res.status(200).json(result);
+  } else {
+    res.status(400).send("Client ID not found");
+  }
+};
+
+const handleDeleteClient = (req, res) => {
+  const id = req.params.id;
+  const deletedClientIndex = clients.findIndex((client) => {
+    return client.id === id;
+  });
+  clients.splice(deletedClientIndex, 1);
+  res.status(200).send("The Client has been deleted");
+};
+
+module.exports = {
+  handleClients,
+  handleAddClient,
+  handleClient,
+  handleDeleteClient,
+};
--- server.js
+++ server.js
@@ -1,23 +1,39 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const clients = require("./data/clients");
+const { response } = require("express");
+const {
+  handleClients,
+  handleAddClient,
+  handleClient,
+  handleDeleteClient,
+} = require("./handlers/clientHandlers");
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
-  // endpoints
+  .get("/clients", handleClients)
+  .post("/clients", handleAddClient)
+  .get("/clients/:id", handleClient)
+  .delete("/clients/:id", handleDeleteClient)
+
+  .get("/", (req, res) => {
+    res.status(404);
+    res.send("Unable to find clients");
+  })
 
   .listen(8000, () => console.log(`Listening on port 8000`));
